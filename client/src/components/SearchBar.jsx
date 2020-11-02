ad724f8946301bcca0c0f31e760e452be0f4bf83
Juan Jose Puente Scapolatempo <juanpuentescapoltempo@gmail.com>
2020-11-02 16:27:31 -0300

    SearchBar

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -8,7 +8,7 @@ export default function SearchBar() {
 
   // HOOKS
   const [effect, setEffect] = useState(true);
-  const [query, setQuery] = useState('Iphone');
+  const [query, setQuery] = useState('');
 
   // HANDLERS
   function handleOnChange(event) {
@@ -21,8 +21,24 @@ export default function SearchBar() {
 
   return (
     <div>
-      <input type="text" name="query" id="query" onChange={handleOnChange} value={query}/>
-      <button onClick={() => handleFetchQuery()}>Buscar</button>
+      <nav className="navbar navbar-expand-sm navbar-primary bg-primary">
+        <a className="navbar-brand text-white">Catalogo</a>
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a className="nav-link text-white" target="_blank" href="https://github.com/josescap2">Github <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" target="_blank" href="https://www.linkedin.com/in/josescap/">Linkedin</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" target="_blank" href="https://www.soyhenry.com/webfullstack/">Henry</a>
+          </li>
+        </ul>
+        <div className="form-inline my-2 my-lg-0">
+          <input className="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search" value={query} onChange={handleOnChange} />
+          <button className="btn btn-outline-light my-2 my-sm-0" onClick={() => handleFetchQuery()}>Search</button>
+        </div>
+      </nav>
     </div>
   );
 }
